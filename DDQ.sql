9a24a1ad5f32db886538aa7b4877a99c86daf3b6
pittenja <55713063+pittenja@users.noreply.github.com>
2021-02-08 11:21:32 -0800

    added comments for query plan

--- DDQ.sql
+++ DDQ.sql
@@ -1,18 +1,36 @@
 --SQL FILE FOR DATABASE CREATION AND INSERTION OF SAMPLE DATA
 
 --Customers Entity - RYAN
+--Table creation:
+
+--Sample data:
 
 
 --RepairJobs Entity - RYAN
+--Table creation:
+
+--Sample data:
 
 
 --Employees Entity - RYAN
+--Table creation:
+
+--Sample data:
 
 
 --BikeModels Entity - JAY
+--Table creation:
+
+--Sample data:
 
 
 --Parts Entity - JAY
+--Table creation:
+
+--Sample data:
 
 
 --BikePartCompatibility Entity - JAY
+--Table creation:
+
+--Sample data:
--- DMQ.sql
+++ DMQ.sql
@@ -1,15 +1,38 @@
 --SQL FILE FOR WEB APP DATABASE MANIPULATION FUNCTIONALITY
 
---Customers html page - RYAN
+--CUSTOMERS HTML PAGE - RYAN--
 
 
---repairs html page - RYAN
+--REPAIRS HTML PAGE - RYAN--
 
 
---employees html page - RYAN
+--EMPLOYEES HTML PAGE - RYAN--
 
 
---bikes html page - JAY
+--BIKES HTML PAGE - JAY--
+--select bikeModels table for viewing
 
+--view compatible parts for a bike model
+----select to bikePartCompatibility to get ids of all compatible parts
+----select queries for each compatible part to get name of part for Bike model part compatbility detail
 
---parts html page - JAY
+--insert new bike model
+----make select to parts table to populate insert form with all available parts
+----upon submit, perform insert into bikeModels table as well as bikePartCompatibility table
+
+--PARTS HTML PAGE - JAY--
+--select parts table for viewing
+
+--delete part
+
+--insert part
+----make select to bikeModels table to populate insert form with all serviceable bikes
+----upon submit, perform insert into parts table as well as bikePartCompatibility table
+
+--update part
+----make select to bikeModels table to populate update form with all serviceable bikes
+----make select query to bikePartCompatibility table to get bike_ids of currently compatible bikes to part so that 
+----form check boxes can be preselected for compatible bikes
+----upon submit, perform update into parts table 
+------perform delete to all rows in bikePartCompatibility that contain the part_id to reset table
+------perform insert of rows into bikePartCompatibility for the updated list of compatible bikes for that part
