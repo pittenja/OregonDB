7cddc69df390da116c78050bb2ce90168c0834bf
pittenja <55713063+pittenja@users.noreply.github.com>
2021-02-09 09:04:57 -0800

    added bikepartcompatibility table and sample data

--- DDQ.sql
+++ DDQ.sql
@@ -50,8 +50,48 @@ partName = 'XL Grips';
 
 --BikePartCompatibility Entity - JAY
 --Table creation:
+CREATE TABLE `BikePartCompatibility` (
+`compatibilityId` int(11) NOT NULL AUTO_INCREMENT,
+`bikeId` int(11),
+`partId` int(11),
+PRIMARY KEY (`compatibilityId`),
+CONSTRAINT `BikePartCompatibility_ibfk_1` FOREIGN KEY (`bikeId`) REFERENCES `BikeModels` (`bikeId`),
+CONSTRAINT `BikePartCompatibility_ibfk_2` FOREIGN KEY (`partId`) REFERENCES `Parts` (`partId`)
+) ENGINE=InnoDB;
 
 --Sample data:
+INSERT INTO BikePartCompatibility
+SET
+bikeId = 
+(SELECT bikeId from BikeModels
+WHERE make = 'Specialized' AND model = 'P1' AND year = '2010'),
+partId = 
+(SELECT partId from Parts
+WHERE partName = '23T Sprocket');
+INSERT INTO BikePartCompatibility
+SET
+bikeId = 
+(SELECT bikeId from BikeModels
+WHERE make = 'Trek' AND model = 'Ticket' AND year = '2012'),
+partId = 
+(SELECT partId from Parts
+WHERE partName = '23T Sprocket');
+INSERT INTO BikePartCompatibility
+SET
+bikeId = 
+(SELECT bikeId from BikeModels
+WHERE make = 'Transition' AND model = 'PBJ' AND year = '2016'),
+partId = 
+(SELECT partId from Parts
+WHERE partName = 'Medium Chain');
+INSERT INTO BikePartCompatibility
+SET
+bikeId = 
+(SELECT bikeId from BikeModels
+WHERE make = 'Transition' AND model = 'PBJ' AND year = '2016'),
+partId = 
+(SELECT partId from Parts
+WHERE partName = 'XL Grips');
 
 
 --Customers Entity - RYAN
