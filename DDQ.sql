7113f327f8af91cde832b1c9cea2c7c291ca1b97
github99er <ryanmurphycorp@protonmail.com>
2021-02-10 15:05:04 -0800

    Creates Customers table

--- DDQ.sql
+++ DDQ.sql
@@ -96,6 +96,12 @@ WHERE partName = 'XL Grips');
 
 --Customers Entity - RYAN
 --Table creation:
+CREATE TABLE `Customers` (
+`customerId` int(11) NOT NULL AUTO_INCREMENT,
+`firstName` varchar(255) NOT NULL,
+`lastName` varchar(255) NOT NULL,
+PRIMARY KEY (`customerId`)
+) ENGINE=InnoDB;
 
 --Sample data:
 
