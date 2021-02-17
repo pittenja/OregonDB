7f69b3084a2bd1af130a8e7c6faf0c792cf7c36f
github99er <ryanmurphycorp@protonmail.com>
2021-02-17 06:17:51 -0800

    Updates Employees view and corresponding JS script

---
+++ public/employeesScript.js
@@ -0,0 +1,31 @@
+function insert(event) {
+    event.preventDefault();
+    var employeeFirstName = document.getElementById("employeeFirstName");
+    var employeeLastName = document.getElementById("employeeLastName");
+  
+    var item = {
+      employeeFirstName: employeeFirstName.value,
+      employeeLastName: employeeLastName.value
+      };
+  
+    //console.log(item);
+    var response = postData('/insert-employees', item).then(data =>{renderData(data);});
+    //console.log(response);
+    document.getElementById("employeeFirstName").value = ""
+    document.getElementById("employeeLastName").value = ""
+  }
+  
+  const form = document.getElementById('employee-insert-form');
+  form.addEventListener('submit', insert);
+  
+  async function postData(url = '', data = {}) {
+      const response = await fetch(url, {
+      method: 'POST', 
+      headers: {
+        'Content-Type': 'application/json'
+           },
+      
+     body: JSON.stringify(data) 
+    });
+    return response.json(); 
+  }
