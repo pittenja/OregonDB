48270342895e76d22475fb4cc3dbe9f9b948e510
github99er <ryanmurphycorp@protonmail.com>
2021-02-17 06:14:19 -0800

    Updates Customers view and corresponding JS script

---
+++ public/customersScript.js
@@ -0,0 +1,34 @@
+function insert(event) {
+    event.preventDefault();
+    var firstName = document.getElementById("firstName");
+    var lastName = document.getElementById("lastName");
+    var email = document.getElementById("email");
+  
+    var item = {
+      firstName: firstName.value,
+      lastName: lastName.value,
+      email: email.value
+      };
+  
+    console.log(item);
+    var response = postData('/insert-customers', item).then(data =>{renderData(data);});
+    //console.log(response);
+    document.getElementById("firstName").value = ""
+    document.getElementById("lastName").value = ""
+    document.getElementById("email").value = ""
+  }
+  
+  const form = document.getElementById('customer-insert-form');
+  form.addEventListener('submit', insert);
+  
+  async function postData(url = '', data = {}) {
+      const response = await fetch(url, {
+      method: 'POST', 
+      headers: {
+        'Content-Type': 'application/json'
+           },
+      
+     body: JSON.stringify(data) 
+    });
+    return response.json(); 
+  }
